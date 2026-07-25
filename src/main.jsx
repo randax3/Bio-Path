a824ba01639806f83436572e9f98cc543380c9f5
randa alwadai <s202338470@kfupm.edu.sa>
2026-07-25 10:29:42 +0300

    تا

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import StudentPortal from './StudentPortal.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StudentPortal />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 )
