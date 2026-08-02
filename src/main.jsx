a765d74bcfe016be39d6771293f3d9e8ffc8e158
randa alwadai <s202338470@kfupm.edu.sa>
2026-08-02 15:02:44 +0300

    ل

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import StudentPortal from './StudentPortal.jsx'
+import StudentPortal from './StudentPortal'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
